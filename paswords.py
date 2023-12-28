5db4d2420dd79b86ed4e77d2916a3a58e1e5079d
paolazambranos <pvzambrano.24@est.ucab.edu.ve>
2023-12-28 14:13:51 -0400

    leer archivo de passwords

--- paswords.py
+++ paswords.py
@@ -1,1 +1,8 @@
-print ("hello world")
+def leer_archivo():
+    archivo_passwords = open("passwords.txt", "r")
+    for linea in archivo_passwords:
+        print(linea)
+
+print ("Bienvenidos al proyecto de Paola Zambrano")
+leer_archivo()
+print("El proyecto ha llegado a su fin y ha sido ejecutado con exito")

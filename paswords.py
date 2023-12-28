f950a6b4a4c229db498fdcbd11ec26bea2a7f67e
paolazambranos <pvzambrano.24@est.ucab.edu.ve>
2023-12-28 15:46:57 -0400

    enviando cada contraseña a la funcion de calculo de puntaje de seguridad

--- paswords.py
+++ paswords.py
@@ -9,8 +9,17 @@ def leer_archivo_passwords(nombre_archivo):
     print("La cantidad de lineas son: ", len(lineas))
     return lineas
 
+def calcular_puntaje_seguridad(clave):
+    print("Comenzamos a calcular el puntaje de seguridad para la clave: ", clave)
+    return 0
+
+
 
 print ("Bienvenidos al proyecto de Paola Zambrano")
 passwords = leer_archivo_passwords("passwords.txt")
 patterns = leer_archivo_passwords("patterns.txt")
+
+for clave in passwords: 
+    calcular_puntaje_seguridad(clave)
+
 print("El proyecto ha llegado a su fin y ha sido ejecutado con exito")

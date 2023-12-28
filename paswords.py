c31c47bbfdfad5ed93901a4cb023b57d1f746b91
paolazambranos <pvzambrano.24@est.ucab.edu.ve>
2023-12-28 15:00:26 -0400

    leer los archivos passwords y patterns y almacenarlos en un arreglo

--- paswords.py
+++ paswords.py
@@ -1,14 +1,16 @@
-def leer_archivo_passwords():
-    print("Comenzamos a leer el archivo de contraseñas")
-    archivo_passwords = open("passwords.txt", "r")
-    passwords = []
+def leer_archivo_passwords(nombre_archivo):
+    print("Comenzamos a leer el archivo: ", nombre_archivo)
+    archivo = open(nombre_archivo, "r")
+    lineas = []
     
-    for linea in archivo_passwords:
-        passwords.append(linea)
+    for linea in archivo:
+        lineas.append(linea)
    
-    #print("arreglo de contraseñas: ", passwords)
-    print("La cantidad de contraseñas a evaluar son: ", len(passwords))
+    print("La cantidad de lineas son: ", len(lineas))
+    return lineas
+
 
 print ("Bienvenidos al proyecto de Paola Zambrano")
-leer_archivo_passwords()
+passwords = leer_archivo_passwords("passwords.txt")
+patterns = leer_archivo_passwords("patterns.txt")
 print("El proyecto ha llegado a su fin y ha sido ejecutado con exito")

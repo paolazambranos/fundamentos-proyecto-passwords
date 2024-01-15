d56421cdb3ad4060cf921f7ab2bb163d034da442
paolazambranos <pvzambrano.24@est.ucab.edu.ve>
2024-01-15 12:28:58 -0400

    Agregar archivos actualizados

---
+++ Proyecto1.py
@@ -0,0 +1,2 @@
+passwords = leer_archivo_passwords("Contraseñas - Proyecto (Fundamentos de Programación SEM202415).txt")
+patterns = leer_archivo_passwords("Patrones obvios de contraseña - Proyecto (Fundamentos de Programación SEM202415).txt")
